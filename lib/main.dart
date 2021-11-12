3008ab3c9cc4e118015f2fba804a90d8d1bcd9cb
Alvaro G <alvaro.guajardo02@inacapmail.cl>
2021-11-12 00:01:31 -0300

    Portada terminada - habilitacion de imagen local en yaml - Cambio del Main

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,95 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Miapp'),
-      ),
-      body: ListView(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Image.network('https://depor.com/resizer/4LiA3UcZpkTbq0pGF8j9dPiahkw=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/6Y2EDIISGFGVFANEVDCR5LCG34.jpg'),
-          ),
-        ],
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Stack(
+          children: [
+            lienzoDerechoPosicion(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Image.asset(
+                    "assets/img/logo.png",
+                    width: MediaQuery.of(context).size.width / 1,
+                    height: 230,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 100),
+                ),
+                texto(),
+                Padding(
+                  padding: EdgeInsets.only(top: 50),
+                ),
+                botonEntrar()
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget texto() {
+  return Container(
+    child: Text(
+      'Bienvenido a Hello',
+      style: TextStyle(
+          color: Colors.white,
+          fontStyle: FontStyle.italic,
+          fontSize: 16,
+          letterSpacing: 4),
+    ),
+  );
+}
+
+Widget botonEntrar() {
+  return ElevatedButton(
+    onPressed: () {},
+    child: Icon(
+      Icons.chevron_right,
+      color: Colors.white,
+    ),
+    style: ElevatedButton.styleFrom(
+      primary: Colors.red,
+      shape: CircleBorder(),
+      padding: EdgeInsets.all(13),
+    ),
+  );
+}
+
+Widget lienzoDerechoPosicion() {
+  return Positioned(
+      child: Align(
+    alignment: FractionalOffset.bottomRight,
+    child: Container(
+        padding: EdgeInsets.only(right: 15, left: 5, top: 50, bottom: 50),
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(200),
+          ),
+        ),
+        child: lienzoDerecho()),
+  ));
+}
+
+Widget lienzoDerecho() {
+  return RotatedBox(
+    quarterTurns: 3,
+    child: Padding(
+      padding: EdgeInsets.only(top: 10),
+      child: Text(
+        'Los mejores autos',
+        style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+            fontSize: 18,
+            letterSpacing: 4),
+      ),
+    ),
+  );
 }
