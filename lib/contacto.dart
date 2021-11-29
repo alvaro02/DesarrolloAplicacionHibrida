3fb800aac2d2e84415caf8a0e99051166955e84a
Alvaro G <alvaro.guajardo02@inacapmail.cl>
2021-11-29 18:14:01 -0300

    formulario terminado - detalles revisar las validadciones

--- lib/contacto.dart
+++ lib/contacto.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class formularioContacto extends StatefulWidget {
   formularioContacto({Key? key}) : super(key: key);
@@ -21,22 +22,101 @@ class _formularioContactoState extends State<formularioContacto> {
       ),
       body: Container(
         padding: const EdgeInsets.only(left: 40, right: 40),
-        key: formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: height * 0.04,
-            ),
-            Text(
-              'Cotiza',
-              style: TextStyle(fontStyle: FontStyle.italic, fontSize: 25, color: Color(0XDD000000,)),
-            ),
-            Text(
-              'tu automovil',
-              style: TextStyle(fontSize: 30, color: Color(0XFFD32F2F)),
-            ),
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: height * 0.04,
+              ),
+              Text(
+                'Cotiza',
+                style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 25,
+                    color: Color(
+                      0XDD000000,
+                    )),
+              ),
+              Text(
+                'Tu automovil',
+                style: TextStyle(fontSize: 30, color: Color(0XFFD32F2F)),
+              ),
+              SizedBox(
+                height: height * 0.05,
+              ),
+              TextFormField(
+                decoration:
+                    InputDecoration(labelText: "Ingrese su nombre completo"),
+                validator: (value) {
+                  if (value!.isEmpty ||
+                      !RegExp(r'^[a-z A-Z]+$').hasMatch(value!)) {
+                    return "Por favor ingrese un nombre";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(
+                height: height * 0.05,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Ingrese su email"),
+                validator: (value) {
+                  if (value!.isEmpty ||
+                      !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}')
+                          .hasMatch(value!)) {
+                    return "Por favor ingrese un email correcto";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(
+                height: height * 0.05,
+              ),
+              TextFormField(
+                decoration:
+                    InputDecoration(labelText: "Ingrese su numero de telefono"),
+                validator: (value) {
+                  if (value!.isEmpty ) {
+                    return "Por favor ingrese numero correcto";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(
+                height: height * 0.05,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Enviar",
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontSize: 22,
+                        color: Color(0XFFEF5350),
+                      )),
+                  NeumorphicButton(
+                    margin: EdgeInsets.only(top: 12),
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        final snackBar =
+                            SnackBar(content: Text('Enviando Formulario'));
+                        _scaffoldKey.currentState!.showSnackBar(snackBar);
+                      }
+                    },
+                    style: NeumorphicStyle(
+                      shape: NeumorphicShape.flat,
+                      boxShape: NeumorphicBoxShape.circle(),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
